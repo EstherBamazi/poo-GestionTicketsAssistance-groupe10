b2ff2832273c1ad0a69ac18e5080d8856aef39b8
Esther Bamazi <bamazi.esther@gmail.com>
2026-09-10 07:20:56 +0000

    rectification de l'abscence de la fonction getNOm dans la classe Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -9,5 +9,9 @@ public class Utilisateur {
         this.nom = nom;
         this.adrresse_electronique = adrresse_electronique;
     }
+
+    public String getNom() {
+        return nom;
+    }
 }
 
