d834c3e23cda48faeb37a6773540f01d6229e058
Cedric-devlab <kaborec496@gmail.com>
2026-09-13 21:51:24 +0000

    Add files via upload

---
+++ TicketDemandeService.java
@@ -0,0 +1,20 @@
+public class TicketDemandeService extends Ticket {
+    private String serviceDemande;
+
+    public TicketDemandeService(String titre, String description, String priorite,
+                                Utilisateur auteur, Lieu lieu, String serviceDemande) {
+        super(titre, description, priorite, auteur, lieu);
+        this.serviceDemande = serviceDemande;
+    }
+
+    @Override
+    public int delaiCibleHeures() {
+        return 24;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | service : " + serviceDemande;
+    }
+}
+
